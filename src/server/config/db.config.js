8e872530f14cf8cadc3c666a2022566f702c8efd
shahjalpen <120688028+shahjalpen@users.noreply.github.com>
2023-01-07 20:12:11 +0000

    Update db.config.js

--- src/server/config/db.config.js
+++ src/server/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   url:
-    "mongodb+srv://mongouser:mongopass@cluster0.gf3x0.gcp.mongodb.net/kitab?retryWrites=true&w=majority",
+    "mongodb+srv://mongouser:<password>@<server>/kitab?retryWrites=true&w=majority",
 };
